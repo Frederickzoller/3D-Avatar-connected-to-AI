e3e1299c115ad80e9cefb962cda2d22033728460
Frederickzoller <info@thedac.info>
2024-11-04 09:38:43 +0100

    Fix root URL and update deployment

--- citizens_llm_chat/urls.py
+++ citizens_llm_chat/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from chat.views import CustomAuthToken
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/chat/', permanent=True)),  # Redirect root to /chat/
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
 ] 
